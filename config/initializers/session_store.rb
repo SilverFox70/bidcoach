17fbfa1b8ce95f304063d74eeb8934eba664ced9
SilverFox70 <wfbrinkert@gmail.com>
2016-06-24 09:39:46 -0700

    Update some small things

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_CMWA'
+Rails.application.config.session_store :cookie_store, key: '_Rails_with_admin_and_auth'
